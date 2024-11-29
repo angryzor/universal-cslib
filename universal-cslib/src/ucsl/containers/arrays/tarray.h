9d93169c76934d8e53c854a25638630319790249
Ruben Tytgat <ruben_tytgat@hotmail.com>
2024-11-29 16:47:33 +0100

    Fix TArray allocator

--- universal-cslib/src/ucsl/containers/arrays/tarray.h
+++ universal-cslib/src/ucsl/containers/arrays/tarray.h
@@ -2,18 +2,19 @@
 #include "array-base.h"
 
 namespace ucsl::containers::arrays {
-	namespace internal {
-		template<typename AllocatorSystem, typename A>
-		struct TArrayArrayAllocatorSystem {
-			using allocator_type = typename AllocatorSystem::allocator_type;
-
-			inline static allocator_type* get_allocator(const A& arr) { return AllocatorSystem::get_allocator(); }
-		};
-	}
+	template<typename T, typename AllocatorSystem>
+	class TArray;
 
 	template<typename T, typename AllocatorSystem>
-	class TArray : public ArrayBase<T, size_t, internal::TArrayArrayAllocatorSystem<AllocatorSystem, TArray<T, AllocatorSystem>>> {
+	struct TArrayArrayAllocatorSystem {
+		using allocator_type = typename AllocatorSystem::allocator_type;
+
+		inline static allocator_type* get_allocator(const ArrayBase<T, size_t, TArrayArrayAllocatorSystem<T, AllocatorSystem>>& arr) { return AllocatorSystem::get_allocator(); }
+	};
+
+	template<typename T, typename AllocatorSystem>
+	class TArray : public ArrayBase<T, size_t, TArrayArrayAllocatorSystem<T, AllocatorSystem>> {
 	public:
-		using ArrayBase<T, size_t, internal::TArrayArrayAllocatorSystem<AllocatorSystem, TArray<T, AllocatorSystem>>>::ArrayBase;
+		using ArrayBase<T, size_t, TArrayArrayAllocatorSystem<T, AllocatorSystem>>::ArrayBase;
 	};
 }
