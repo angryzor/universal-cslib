ed8f8995a64565ab9331990e5dd7407e453168fe
angryzor <ruben_tytgat@hotmail.com>
2026-03-27 19:41:26 +0100

    more cemt

--- universal-cslib/src/ucsl/resources/cemt/v100000.h
+++ universal-cslib/src/ucsl/resources/cemt/v100000.h
@@ -11,6 +11,12 @@ namespace ucsl::resources::cemt::v100000 {
 		float jitter;
 	};
 
+	struct JitteredVector3 {
+		JitteredValue x;
+		JitteredValue y;
+		JitteredValue z;
+	};
+
 	struct Unk2Data {};
 
 	struct Unk3Data {};
@@ -69,10 +75,15 @@ namespace ucsl::resources::cemt::v100000 {
 	};
 
 	struct AnimationParam {
-		char flags;
+		enum class LoopType : unsigned char { // completely guessed
+			NORMAL,
+			BOUNCE,
+		};
+
+		bool useGlobalTime;
 		char unk1;
-		char unk2;
-		bool durationEnabled;
+		LoopType loopType;
+		bool timeScale;
 		int loopCount;
 		short totalFrameCount;
 		unsigned short trackCount;
@@ -97,7 +108,7 @@ namespace ucsl::resources::cemt::v100000 {
 		char name[128];
 	};
 
-	struct ModifierParam {
+	struct FieldParam {
 		enum class Type : unsigned char {
 			GRAVITY,
 			SPEED,
@@ -232,74 +243,101 @@ namespace ucsl::resources::cemt::v100000 {
 	};
 
 	struct TextureParam {
-		enum class UVFlags {
+		enum class TextureFlag : unsigned int {
+			TEXTURE_TYPE_0,
+			TEXTURE_TYPE_1,
+			TEXTURE_TYPE_2,
+			TEXTURE_TYPE_3,
+			TEXTURE_TYPE_4,
+			TEXTURE_TYPE_5,
+			TEXTURE_TYPE_6,
+			TEXTURE_TYPE_7,
+			TEXTURE_TYPE_8,
+			TEXTURE_TYPE_9,
+		};
+
+		enum class AnimationFlag : unsigned int {
 			ANIMATED, //0x01
 			UNK1, //0x02
 			UNK2, //0x04
-			UNK3, //0x08
+			STEPWISE_PATTERN_ANIMATION, //0x08
 			ENABLE_SECONDARY_SCROLL, //0x10
 			UNK5, //0x20
-			USE_RANDOM_UNK0, //0x40
-			UNK7, //0x80
+			RANDOMIZE_STARTING_FRAMES, //0x40
+			ENABLE_PATTERN_ANIMATION, //0x80
 			UNK8, //0x100
 			SCROLL_U, //0x200
 			SCROLL_V, //0x400
 			UNK11, //0x800
-			UNK12, //0x1000 - tested at 0x140FF4598 in frontiers 1.42
-			UNK13, //0x2000
+			USE_MATRICES_ON_TEXTURE_TYPE_0, //0x1000 - tested at 0x140FF4598 in frontiers 1.42
+			USE_MATRICES_ON_TEXTURE_TYPE_1, //0x2000
 			SCALE_U, //0x4000
 			SCALE_V //0x8000
 		};
 
-		struct ScrollColorParam {
-			float scrollColorX;
-			float scrollColorY;
-			float scrollStartJitterX;
-			float scrollStartJitterY;
-		};
-
-		struct ScrollUVParam {
-			float tilingX;
-			float tilingY;
-			float rotation;
-			float offsetX;
-			float offsetY;
-			int unk15;
-			PtrData<AnimationParam> tilingAnimation;
-			PtrData<AnimationParam> rotationAnimation;
-			PtrData<AnimationParam> offsetAnimation;
-		};
-
-		enum class ScrollDirectionFlag : unsigned char {
+		enum class DirectionFlag : unsigned char {
 			INVERT_X,
 			INVERT_Y,
 		};
 
-		enum class ScrollDirectionRandomizationFlag : unsigned char {
+		enum class DirectionRandomizationFlag : unsigned char {
 			RANDOMIZE_X,
 			RANDOMIZE_Y,
 		};
 
+		struct ScrollColorParam {
+			ucsl::math::Vector2 scrollColor;
+			ucsl::math::Vector2 scrollStartJitter;
+		};
+
+		struct UVAnimationParam {
+			ucsl::math::Vector2 scale;
+			float rotation;
+			ucsl::math::Vector2 translation;
+			int gap15;
+			PtrData<AnimationParam> tilingAnimation;
+			PtrData<AnimationParam> rotationAnimation;
+			PtrData<AnimationParam> translationAnimation;
+		};
+
+		struct PatternAnimationParam {
+			enum class Type : unsigned char {
+				UNK0,
+				UNK1,
+				UNK2,
+				UNK3,
+				UNK4,
+				UNK5,
+				UNK6,
+				UNK7,
+				UNK8,
+				UNK9,
+				UNK10,
+			};
+
+			unsigned char startingFrame;
+			unsigned char stepCountX;
+			unsigned char stepCountY;
+			Type type;
+			unsigned char frameRangeStart;
+			unsigned char frameRangeEnd;
+			unsigned char frameCount;
+			unsigned char repeating;
+			char unk6;
+			char unk6a;
+			ucsl::bits::Bitset<DirectionFlag> scrollDirectionFlags;
+			ucsl::bits::Bitset<DirectionRandomizationFlag> scrollDirectionRandomizationFlags;
+			unsigned char unk9;
+			PtrData<AnimationParam> patternAnimation;
+		};
+
 		char name[128];
-		int flags0;
-		csl::ut::Bitset<UVFlags> flags1;
-		unsigned char unk0; // unk452434 on the live element
-		unsigned char patternAnimXSteps;
-		unsigned char patternAnimYSteps;
-		unsigned char unk3;
-		unsigned char unk0RandomRangeStart;
-		unsigned char unk0RandomRangeEnd;
-		unsigned char unk5;
-		unsigned char unk5a;
-		char unk6;
-		char unk6a;
-		ucsl::bits::Bitset<ScrollDirectionFlag> scrollDirectionFlags;
-		ucsl::bits::Bitset<ScrollDirectionRandomizationFlag> scrollDirectionRandomizationFlags;
-		unsigned char unk9;
-		PtrData<AnimationParam> patternAnim;
+		ucsl::bits::Bitset<TextureFlag> textureFlags;
+		ucsl::bits::Bitset<AnimationFlag> animationFlags;
+		PatternAnimationParam patternAnimationParam;
 		ScrollColorParam scrollColorParams[2];
-		ScrollUVParam uvParams[2];
-		float uvScale[2];
+		UVAnimationParam uvAnimationParams[2];
+		ucsl::math::Vector2 uvScale;
 		int64_t unk22;
 	};
 
@@ -424,7 +462,8 @@ namespace ucsl::resources::cemt::v100000 {
 			char pad[3];
 			unsigned int unk1;
 			unsigned char unk4;
-			char pad1[0x2];
+			unsigned char unk5;
+			unsigned char unk6;
 			bool unk2;
 			char pad2[0x10];
 		};
@@ -433,7 +472,8 @@ namespace ucsl::resources::cemt::v100000 {
 			unsigned int type;
 			char pad[4];
 			unsigned char unk1;
-			char pad1[0x5];
+			char pad1[0x4];
+			unsigned char unk3;
 			bool unk2;
 			char pad2[0x11];
 		};
@@ -455,32 +495,56 @@ namespace ucsl::resources::cemt::v100000 {
 
 		enum class GpuParticleFlag : unsigned int {
 			IS_GPU,
+			UNK1,
+			UNK2,
 		};
 
-		float initialRotation[6];
-		unsigned int unk18a0;
-		char gap6aa2[24];
-		unsigned int unkVec2Flags;
-		unsigned char unkVec2OffsetXRandomFlags;
-		unsigned char unkVec2OffsetYRandomFlags;
-		unsigned char unkVec2OffsetZRandomFlags;
-		float angularVelocity[6];
-		unsigned int unk18a01;
-		JitteredValue sizeX;
-		JitteredValue sizeY;
-		JitteredValue sizeZ;
-		unsigned int sizeFlags; //controls whetever jiggle should be used
-		JitteredValue scaleX;
-		JitteredValue scaleY;
-		JitteredValue scaleZ;
-		unsigned int scaleFlags; //controls whetever jiggle should be used
+		struct DirectionParam {
+			enum class RandomAngleFlag : unsigned char {
+				CCW_90,
+				CW_90,
+				CW_180,
+				CW_0
+			};
+
+			JitteredVector3 rotation;
+			unsigned int rotationFlags;
+			JitteredVector3 randomizedDirectionRotation;
+			unsigned int randomizedDirectionRotationFlags;
+			ucsl::bits::Bitset<RandomAngleFlag> rotationRandomInitialAngleFlags[3];
+			JitteredVector3 angularVelocity;
+			unsigned int angularVelocityFlags;
+			JitteredVector3 size;
+			unsigned int sizeFlags;
+			JitteredVector3 scale;
+			unsigned int scaleFlags;
+		};
+
+		enum class UpdateFlag : unsigned int {
+			HAS_CHILDREN, // 0x1
+			HAS_RANDOMIZED_ROTATION_DIRECTION, // 0x2
+			HAS_ANGRULAR_VELOCITY, // 0x4
+			USE_GLOBAL_TIME, // 0x8
+			HAS_MODEL, // 0x10
+			UNK5, // 0x20
+			UNK6, // 0x40
+			UNK7, // 0x80
+			UNK8, // 0x100
+			UNK9, // 0x200
+			UNK10, // 0x400
+			DISABLE_ANIMATIONS, // 0x800
+			UNK12, // 0x1000
+			DISABLE_FIELDS, // 0x2000
+		};
+
+		DirectionParam directionParam;
 		ColorParam colorParams[2];
-		JitteredValue fps;
-		PtrData<AnimationParam> unkVec2Anim;
+		JitteredValue lifetime;
+		PtrData<AnimationParam> rotationAnimation;
 		PtrData<AnimationParam> scaleAnimation;
 		PtrData<AnimationParam> sizeAnimation;
 		char gap6aa[0xC];
-		bool useModel;
+		bool hasModel;
 		unsigned char modelSetting1;
 		char gap6b[128];
 		char modelName[128];
@@ -492,19 +556,20 @@ namespace ucsl::resources::cemt::v100000 {
 		TextureParam textures[4];
 		unsigned int textureCount;
 		ChildEffect childEffects[16];
-		ModifierParam modifiers[8];
+		FieldParam fields[8];
 		PtrData<AnimationParam> modifierAnimations[5][8];
-		unsigned int fieldFlags; // 0x01 = has childeffects, 0x4 = use simple unkVec2 update (only x multiplier, no anim), 0x8 = use emitter global time for unkVec2 update, 0x1000 = use -1 or fps variables
-		char gap7b[0x4];
+		ucsl::bits::Bitset<UpdateFlag> updateFlags; // 0x01 = has childeffects, 0x4 = use simple unkVec2 update (only x multiplier, no anim), 0x8 = use emitter global time for unkVec2 update, 0x1000 = use -1 or fps variables
+		unsigned int gap7b;
 		PtrData<AnimationParam> unkAnim7bc;
 		unsigned int someAnimationCount; // 140FF3B3D
 		char gap7bb[0x34];
 		ucsl::bits::Bitset<GpuParticleFlag> gpuParticleFlags; // 0x1 = is gpu rendering?
 		char vectorFieldName[128];
 		math::Position vectorFieldSize;
-		char gap7c2[0x3C];
+		char gap7c2[0x30];
+		math::Position unkVector2;
 		math::Position vectorFieldParam;
-		char gap7c1[0xC];
+		math::Position unkVector1;
 		math::Position depthCollision;
 		char gap7c[0x10];
 		unsigned int unk7d; // 140FE9F5B
@@ -542,11 +607,11 @@ namespace ucsl::resources::cemt::v100000 {
 				TORUS,
 				CUBE,
 				MESH,
-				FIXED = 8
+				FIXED
 			};
 
 			Shape shape;
-			csl::math::Position randomTransform;
+			ucsl::math::Position randomTransform;
 			float spread;
 			float startAngle;
 			float endAngle;
@@ -584,6 +649,37 @@ namespace ucsl::resources::cemt::v100000 {
 			PtrData<AnimationParam> emissionCountAnimation;
 		};
 
+		struct EmissionDynamicsParam {
+			struct Settings {
+				enum class SpreadDirectionType : unsigned int {
+					DIRECTION,
+					ANGLES,
+				};
+
+				float initialAcceleration;
+				float acceleration;
+				float randomVelocity;
+				float velocity;
+				float shapeRadius;
+				float spreadVelocity;
+				float spread;
+				SpreadDirectionType spreadType;
+				csl::math::Position spreadAnglesOrDirection;
+				float initialVelocityJitter;
+				float velocityJitter;
+			};
+
+			Settings settings;
+			float gap4d;
+			PtrData<AnimationParam> initialAccelerationAnimation;
+			PtrData<AnimationParam> accelerationAnimation;
+			PtrData<AnimationParam> velocityAnimation;
+			PtrData<AnimationParam> shapeRadiusAnimation;
+			PtrData<AnimationParam> velocityScaleAnimation;
+			PtrData<AnimationParam> emitSizeAnimation;
+			PtrData<AnimationParam> emitVectorAnimation;
+		};
+
 		enum class InheritFlag : unsigned int {
 			TRANSLATION_RAW, // ignore world scale
 			ROTATION, // parent rotation
@@ -600,8 +696,8 @@ namespace ucsl::resources::cemt::v100000 {
 			USE_TEXTURES,
 			USE_RENDER_PRIORITY,
 			UNK6,
-			UNK7,
-			UNK8,
+			USE_UNKFLOAT0_RANGE,
+			USE_UNKFLOAT1_RANGE,
 			UNK9,
 			UNK10,
 			UNK11, // sets emitter flag 0x40
@@ -655,9 +751,9 @@ namespace ucsl::resources::cemt::v100000 {
 			UNK2,
 		};
 
-		csl::math::Position position;
-		csl::math::Position rotation;
-		csl::math::Position scale;
+		ucsl::math::Position position;
+		ucsl::math::Position rotation;
+		ucsl::math::Position scale;
 		unsigned int cameraFlags; //changes how the emitter faces the camera (billboard style, using the values etc)
 		PtrData<AnimationParam> unk1Animation;
 		PtrData<AnimationParam> unk2Animation;
@@ -666,25 +762,7 @@ namespace ucsl::resources::cemt::v100000 {
 		EmitParam emitParam;
 		unsigned int unkType;
 		float lifeEndTime;
-		float accelerationMultiplier;
-		float accelerationNormalMultiplier;
-		float initialSpeed;
-		float velocityMultiplier;
-		float shapeRadius;
-		float velocityScale;
-		float emitSize;
-		bool useEmitVector;
-		csl::math::Position emitVector;
-		float emitVectorJitter;
-		float directionJitter;
-		float unk4d;
-		PtrData<AnimationParam> accelerationMultiplierAnimation;
-		PtrData<AnimationParam> accelerationNormalMultiplierAnimation;
-		PtrData<AnimationParam> velocityMultiplierAnimation;
-		PtrData<AnimationParam> shapeRadiusAnimation;
-		PtrData<AnimationParam> velocityScaleAnimation;
-		PtrData<AnimationParam> emitSizeAnimation;
-		PtrData<AnimationParam> emitVectorAnimation;
+		EmissionDynamicsParam emissionDynamicsParam;
 		BlendMode blendMode;
 		DepthMode depthMode;
 		CullMode cullMode;
@@ -695,15 +773,15 @@ namespace ucsl::resources::cemt::v100000 {
 		ucsl::bits::Bitset<UnkFlags> unkFlags;
 		float unkFloat4;
 		float unkFloat5;
-		float unkFloat0;
-		float unkFloat0Jitter;
-		float unkFloat1;
-		float unkFloat1Jitter;
-		float unkFloat2;
-		float unkFloat3;
+		float unkFloat0RangeStart;
+		float unkFloat0RangeEnd;
+		float unkFloat1RangeStart;
+		float unkFloat1RangeEnd;
+		float unkFloat2RangeStart;
+		float unkFloat2RangeEnd;
 		float unkFloat6RangeEnd; // 140FE4F74
 		float unkFloat6RangeStart;
-		char gap6b[0x4];
+		unsigned int gap6b;
 		unsigned int unkInt0;
 		unsigned int unkInt1;
 		unsigned int unkInt2;
@@ -714,7 +792,7 @@ namespace ucsl::resources::cemt::v100000 {
 		unsigned char unk6b4; // 140FE4F0A
 		unsigned char unkType5;
 		BillboardType billboardType; // handled in 0140FE9390
-		bool unkBool0;
+		bool enableUnkFloat2Range;
 		char gap6b2;
 		unsigned char unkChar0;
 		char gap6b3[3];
